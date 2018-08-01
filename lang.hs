d20a62caf321e271eff3b67bcff511d4e00eadec
4afs <g2x649@gmail.com>
2018-08-01 23:14:06 +0000

    fix

--- lang.hs
+++ lang.hs
@@ -1,19 +1,14 @@
 data Token = Plus 
-           | TokenNat Int
+           | TnNat Int
 
-data Term = TermAdd Int Int
-          | TermNat Int
+data Term = TAdd Int Int
+          | TmNat Int
 
 main = do
-    mapM_ print "123++"
+    let formula = words "1 2 3 + +"
+    print formula
 
--- replace :: Char -> Int 
--- replace "+" = 0
--- replace num = read num :: Int
---
--- lexer :: [Char] -> [Int]
--- lexer [] = []
--- lexer (c:source) = replace c : lexer source
+-- mexer :: [Char] -> [Int]
 
 -- parser :: [Token] -> Term
 
